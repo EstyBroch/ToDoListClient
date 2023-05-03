7fe179642f807f6b8ba712ad978f85f71e038d25
esty broch <brochesty@gmail.com>
2023-05-03 16:27:42 +0300

    changeUrl

--- src/service.js
+++ src/service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:5118";
+//axios.defaults.baseURL = "http://localhost:5118";
+axios.defaults.baseURL = process.env.REACT_APP_URL;
 
 axios.interceptors.response.use(
   function(response) {
